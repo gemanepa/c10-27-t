044c9e297573064d87c85df33143abdade680eaf
gemanepa <contact@gemanepa.com>
2023-03-25 12:22:46 -0300

    create components folder and add AssetExample

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-paper';
+import Constants from 'expo-constants';
+import AssetExample from './components/AssetExample';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>NoCountry - c10-27-t</Text>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.paragraph}>NoCountry</Text>
+      <Card>
+        <AssetExample />
+      </Card>
       <StatusBar style="auto" />
     </View>
   );
