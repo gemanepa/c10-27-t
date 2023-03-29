a30805e9fa868d97c9911efebc0770e8918de5be
Oscar Tandioy <tato.tandioy@gmail.com>
2023-03-29 15:42:38 -0500

    feat: Added appearance for amount, account and date entry

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import HomeScreen from './screens/index';
 import DetailsScreen from './screens/details';
 import SettingUpScreen from './screens/initial-setup';
 import useAsyncStorage from './hooks/useAsyncStorage';
+import AddTransaction from './screens/addTransaction';
 
 const Stack = createNativeStackNavigator();
 
@@ -28,6 +29,7 @@ function App() {
             initialParams={{ setInitialSettingUp }}
           />
           <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="AddTransaction" component={AddTransaction} />
         </Stack.Navigator>
       </NavigationContainer>
       <StatusBar style="auto" />
