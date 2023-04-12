27a9e369c0d1c1a2e8b1bb89e4cebd0bf174d1dd
gemanepa <contact@gemanepa.com>
2023-04-12 05:30:03 -0300

    feat: add pin subscreen and design styles for setting up screen

--- screens/initial-setup.js
+++ screens/initial-setup.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 import { Button } from 'react-native-paper';
 import Constants from 'expo-constants';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import PropTypes from 'prop-types';
+
 import Header from '../components/initial-setup/Header';
 import CurrencyDropdown from '../components/initial-setup/CurrencyDropdown';
 import BalanceInput from '../components/initial-setup/BalanceInput';
@@ -11,17 +13,21 @@ import BalanceInput from '../components/initial-setup/BalanceInput';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: 'white',
     gap: 20,
+    paddingTop: Constants.statusBarHeight,
+  },
+  image: {
+    alignSelf: 'center',
+
+    resizeMode: 'center',
   },
   inputsContainer: {
     flex: 1,
-    paddingTop: '5%',
+    paddingTop: '15%',
     paddingHorizontal: '15%',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#DCDCDC',
+    backgroundColor: '#F6F6FD',
     borderTopLeftRadius: 90,
     borderTopRightRadius: 90,
   },
@@ -33,19 +39,23 @@ const styles = StyleSheet.create({
     alignSelf: 'center', // center the text vertically
   },
   text: {
-    fontSize: 16,
-    fontWeight: 500,
+    fontSize: 18,
+    fontWeight: 400,
     textAlign: 'center',
     alignSelf: 'center',
     paddingHorizontal: 30,
     marginBottom: 15,
   },
   button: {
-    width: '100%',
+    width: 248,
     borderRadius: 10,
     padding: 5,
-    backgroundColor: 'gray',
+    backgroundColor: '#FA6C17',
     marginTop: 10,
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 16,
+    fontWeight: 700,
   },
 });
 
@@ -54,6 +64,9 @@ const currencyOptions = ['USD', 'ARS', 'EUR', 'GBP', 'BRL', 'CLP', 'COP', 'MXN',
 export default function SettingUpScreen({ setInitialSettingUp }) {
   const [selectedCurrency, setSelectedCurrency] = useState('');
   const [selectedBalance, setSelectedBalance] = useState('');
+  const [selectedPin, setSelectedPin] = useState('');
+  const [confirmPin, setConfirmedPin] = useState('');
+  const [step, setStep] = useState(1);
 
   const handleCurrencySelect = (value) => {
     setSelectedCurrency(value);
@@ -68,50 +81,133 @@ export default function SettingUpScreen({ setInitialSettingUp }) {
       if (!selectedCurrency || !selectedBalance) return;
       const userCurrency = { currency: selectedCurrency, amount: Number(selectedBalance) };
       await AsyncStorage.setItem('userCurrency', JSON.stringify(userCurrency));
+      setStep(2);
+    } catch (error) {
+      console.error(error); // eslint-disable-line no-console
+    }
+  };
+
+  const storePin = async () => {
+    try {
+      if (!selectedPin || !confirmPin || selectedPin !== confirmPin) return;
+      await AsyncStorage.setItem('userPin', JSON.stringify(selectedPin));
       setInitialSettingUp('success');
     } catch (error) {
       console.error(error); // eslint-disable-line no-console
     }
   };
 
-  return (
-    <View style={styles.container}>
-      <Header />
+  const handleSelectedPin = (val) => {
+    setSelectedPin(val);
+    if (confirmPin) setConfirmedPin('');
+  };
 
-      <View style={styles.inputsContainer}>
-        <View>
-          <View style={styles.inputWithTextContainer}>
-            <Text style={styles.text}>Indica el tipo de divisa</Text>
-            <CurrencyDropdown
-              options={currencyOptions}
-              defaultValue={selectedCurrency}
-              onSelect={handleCurrencySelect}
-            />
-          </View>
+  const headerText = step === 1 ? '¡Organicemos tus finanzas juntos!' : '¡Protejamos tus datos!';
 
-          <View style={styles.inputWithTextContainer}>
-            {selectedCurrency && (
-              <>
-                <Text style={styles.text}>Introduce el balance de la cuenta principal</Text>
-                <BalanceInput
-                  value={selectedBalance}
-                  onChange={handleBalanceSelect}
-                  selectedCurrency={selectedCurrency}
-                />
-              </>
-            )}
-          </View>
+  const stepOneInputs = (
+    <>
+      <View>
+        <View style={styles.inputWithTextContainer}>
+          <Text style={styles.text}>Indica el tipo de divisa</Text>
+          <CurrencyDropdown
+            options={currencyOptions}
+            defaultValue={selectedCurrency}
+            onSelect={handleCurrencySelect}
+          />
         </View>
 
         <View style={styles.inputWithTextContainer}>
-          {selectedCurrency && selectedBalance && (
-            <Button style={styles.button} mode="contained" onPress={storeData}>
-              Continuar
-            </Button>
+          {selectedCurrency && (
+            <>
+              <Text style={styles.text}>Introduce el balance de la cuenta principal</Text>
+              <BalanceInput
+                value={selectedBalance}
+                onChange={handleBalanceSelect}
+                selectedCurrency={selectedCurrency}
+              />
+            </>
           )}
         </View>
       </View>
-    </View>
+      <View style={styles.inputWithTextContainer}>
+        {selectedCurrency && selectedBalance && (
+          <Button style={styles.button} mode="contained" onPress={storeData}>
+            Continuar
+          </Button>
+        )}
+      </View>
+    </>
+  );
+
+  const stepTwoInputs = (
+    <>
+      <View>
+        <View style={styles.inputWithTextContainer}>
+          <Text style={styles.text}>Ingresa un PIN</Text>
+          <TextInput
+            style={{
+              borderWidth: 1,
+              borderColor: '#ccc',
+              borderRadius: 10,
+              backgroundColor: 'white',
+              width: 232,
+              fontSize: 16,
+              textAlign: 'center',
+              height: 50,
+            }}
+            secureTextEntry
+            keyboardType="numeric"
+            value={selectedPin}
+            onChangeText={(value) =>
+              value.toString().length < 7 && handleSelectedPin(value.toString())
+            }
+            placeholder="Ej: 123456"
+          />
+        </View>
+
+        <View style={styles.inputWithTextContainer}>
+          {selectedPin && selectedPin.length === 6 && (
+            <>
+              <Text style={styles.text}>Confirmar PIN</Text>
+              <TextInput
+                style={{
+                  borderWidth: 1,
+                  borderColor: '#ccc',
+                  borderRadius: 10,
+                  backgroundColor: 'white',
+                  width: 232,
+                  fontSize: 16,
+                  textAlign: 'center',
+                  height: 50,
+                }}
+                secureTextEntry
+                keyboardType="numeric"
+                value={confirmPin}
+                onChangeText={(value) =>
+                  value.toString().length < 7 && setConfirmedPin(value.toString())
+                }
+                placeholder="Ej: 123456"
+              />
+            </>
+          )}
+        </View>
+      </View>
+
+      <View style={styles.inputWithTextContainer}>
+        {selectedPin && confirmPin && selectedPin === confirmPin && (
+          <Button style={styles.button} mode="contained" onPress={storePin}>
+            Guardar
+          </Button>
+        )}
+      </View>
+    </>
+  );
+
+  return (
+    <LinearGradient style={styles.container} colors={['#03B263', 'green', '#F6F6FD', '#F6F6FD']}>
+      <Header headerText={headerText} />
+      <View style={styles.inputsContainer}>{step === 1 ? stepOneInputs : stepTwoInputs}</View>
+    </LinearGradient>
   );
 }
 
