c292b2d76a09554bfcf2dbed168af9e5ed5eae0f
gemanepa <contact@gemanepa.com>
2023-04-16 05:08:25 -0300

    feat: add mocked data provider + statistics route + react-native-svg-charts library

--- components/home/expenses-and-incomes-lists/Tables/utils.js
+++ components/home/expenses-and-incomes-lists/Tables/utils.js
@@ -1,14 +1,3 @@
-/* eslint-disable no-plusplus */
-const categories = ['Salud', 'Educacion', 'Higiene', 'Hogar', 'Transporte', 'Comida', 'Ocio'];
-
-const generateRandomDate = () => {
-  const startDate = new Date('2022-12-01').getTime();
-  const endDate = new Date().getTime();
-  const randomTimestamp = Math.floor(Math.random() * (endDate - startDate + 1)) + startDate;
-  const randomDate = new Date(randomTimestamp);
-  return randomDate;
-};
-
 const formatDate = (date) => {
   const day = date.getDate();
   const month = date.toLocaleString('default', { month: 'short' });
@@ -16,29 +5,4 @@ const formatDate = (date) => {
   return `${day} ${month} ${year}`;
 };
 
-const generateRandomAmount = (currency) => {
-  const randomAmount = Math.random() * 50;
-  return `${randomAmount.toFixed(2)} ${currency}`;
-};
-
-const generateRandomTableData = (currency) => {
-  const tableData = [];
-  const usedKeys = new Set();
-
-  for (let i = 0; i < 40; i++) {
-    const category = categories[Math.floor(Math.random() * categories.length)];
-    const date = generateRandomDate();
-    const amount = generateRandomAmount(currency);
-    let key = category;
-    let j = 1;
-    while (usedKeys.has(key)) {
-      key = `${category}-${j}`;
-      j++;
-    }
-    usedKeys.add(key);
-    tableData.push({ key, category, date, amount });
-  }
-  return tableData.sort((a, b) => b.date.getTime() - a.date.getTime());
-};
-
-export { generateRandomTableData, formatDate };
+export { formatDate }; // eslint-disable-line import/prefer-default-export
